278cc2e47eb2ad628bd0152103313f5074c92c8b
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2019-02-21 21:04:06 -0600

    Add header and footer components

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <header className='header'>
+    <h1 className='title'>Aeon&rsquo;s End Randomizer</h1>
+  </header>
+)
+
+export default Header
