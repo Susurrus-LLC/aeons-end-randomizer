fa612c4d56214b02de1a8e3463555151b2d9e898
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2019-02-21 19:13:26 -0600

    Switch to SASS

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-module.exports = function override(config, env) {
+module.exports = function override (config, env) {
   require('react-app-rewire-postcss')(config, {
     plugins: loader => [
       require('postcss-preset-env')(),
