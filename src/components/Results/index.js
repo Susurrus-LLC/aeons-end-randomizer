ace2d260141da6e717a8c9fa252f36050d8f719d
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2019-04-05 09:22:02 -0500

    Handle selecting sets in results

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './results.sass'
 
 const Results = props => {
+  const [setsArr, setSetsArr] = useState([])
+  const sets = props.data ? props.data.sets : null
+  const market = props.data ? props.data.market : null
+  const mages = props.data ? props.data.mages : null
+  const nemesis = props.data ? props.data.nemesis : null
+
+  useEffect(() => {
+    if (props.data) {
+      let newArr = []
+      const setsKeys = Object.keys(sets)
+      setsKeys.forEach(key => {
+        if (sets[key]) {
+          newArr.push(key)
+        }
+      })
+      setSetsArr(newArr)
+    }
+  })
+
   return (
     <div className='results'>
-      Results
+      <p>{JSON.stringify(setsArr)}</p>
+      <p>{JSON.stringify(market)}</p>
+      <p>{JSON.stringify(mages)}</p>
+      <p>{JSON.stringify(nemesis)}</p>
     </div>
   )
 }
