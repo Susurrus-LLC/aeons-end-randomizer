ff04ef4926363c02f7bd43d9cd4ec497b305e88e
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2019-04-08 19:43:53 -0500

    Reduce page copyright to one p element

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -19,8 +19,7 @@ const Footer = () => {
         <p className='ae-copyright'>Aeon&rsquo;s End, by <a href='https://www.indieboardsandcards.com/' target='_blank' rel='noopener noreferrer'>Indie Boards &amp; Cards</a> and <a href='https://www.actionphasegames.com/' target='_blank' rel='noopener noreferrer'>ActionPhase Games</a>, is copyrighted &copy; 2016&ndash;2019 by Lone Oak Games, all rights reserved. The official randomizer app is available on <a href='https://www.actionphasegames.com/pages/aerandomizer' target='_blank' rel='noopener noreferrer'>ActionPhase&rsquo;s website</a>.</p>
       </div>
       <div className='pg-block'>
-        <p className='pg-copyright'>This site was built by <a href='https://github.com/nai888' target='_blank' rel='noopener noreferrer'>Ian A.&nbsp;Cook</a>, copyright &copy; {years()} under the <a href='https://github.com/nai888/aeons-end-randomizer/blob/master/LICENSE' target='_blank' rel='noopener noreferrer'>AGPL-3.0 license</a>.</p>
-        <p className='pg-source'>The source code is available on GitHub at <a href='https://github.com/nai888/aeons-end-randomizer' target='_blank' rel='noopener noreferrer'>nai888/aeons-end-randomizer</a>.</p>
+        <p className='pg-copyright'>This site was built by <a href='https://github.com/nai888' target='_blank' rel='noopener noreferrer'>Ian A.&nbsp;Cook</a>, copyright &copy; {years()} under the <a href='https://github.com/nai888/aeons-end-randomizer/blob/master/LICENSE' target='_blank' rel='noopener noreferrer'>AGPL-3.0 license</a>. The source code is available on GitHub at <a href='https://github.com/nai888/aeons-end-randomizer' target='_blank' rel='noopener noreferrer'>nai888/aeons-end-randomizer</a>.</p>
       </div>
     </footer>
   )
