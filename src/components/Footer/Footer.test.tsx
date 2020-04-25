4575204507fc21ce81fc4a076557b82a5c4800c4
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2020-04-25 10:06:07 -0500

    Simplify Footer copyright, add Link to About

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import Footer from '.'
 
 describe('Footer', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<Footer />, div)
+    ReactDOM.render(
+      <Router>
+        <Footer />
+      </Router>,
+      div
+    )
     ReactDOM.unmountComponentAtNode(div)
   })
 })
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import AppInfo from '../../pages/About/AppInfo'
 
@@ -6,12 +7,7 @@ import styles from './footer.module.sass'
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <div className={styles.block}>
-      <p className={styles.copyright}>Aeon&rsquo;s End, by <a href='https://www.indieboardsandcards.com/' target='_blank' rel='noopener noreferrer'>Indie Boards &amp; Cards</a> and <a href='https://www.actionphasegames.com/' target='_blank' rel='noopener noreferrer'>ActionPhase Games</a>, is copyrighted &copy; 2016&ndash;2019 by Lone Oak Games, all rights reserved. The official randomizer app is available on <a href='https://www.actionphasegames.com/pages/aerandomizer' target='_blank' rel='noopener noreferrer'>ActionPhase&rsquo;s website</a>.</p>
-    </div>
-    <div className={styles.block}>
-      <p className={styles.copyright}>Version {AppInfo.version}, built by <a href={AppInfo.authorUrl} target='_blank' rel='noopener noreferrer'>{AppInfo.author}</a>, copyright &copy; {AppInfo.years()} under the <a href={`${AppInfo.ghUrl}/blob/master/LICENSE`} target='_blank' rel='noopener noreferrer'>AGPL-3.0 license</a>. The source code is available <a href={AppInfo.ghUrl} target='_blank' rel='noopener noreferrer'>on GitHub</a>.</p>
-    </div>
+    <p className={styles.copyright}>Version {AppInfo.version}, built by <a href={AppInfo.authorUrl} target='_blank' rel='noopener noreferrer'>{AppInfo.author}</a>, copyright &copy; {AppInfo.years()} under the <a href={`${AppInfo.ghUrl}/blob/master/LICENSE`} target='_blank' rel='noopener noreferrer'>AGPL-3.0 license</a>. <Link to='/about'>About this app.</Link></p>
   </footer>
 )
 
