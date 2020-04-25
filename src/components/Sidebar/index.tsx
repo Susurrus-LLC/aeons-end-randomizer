8b39457074a5042a1bbbff410004c55cde47b871
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2020-04-25 14:06:05 -0500

    Use modal to cover screen

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, MouseEvent, useRef } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import styles from './sidebar.module.sass'
@@ -8,84 +8,96 @@ const Sidebar = () => {
 
   const toggleOpen = () => setOpen(!open)
 
+  const handleClickOutside = (e: MouseEvent) =>
+    e.target === overlayRef.current ? toggleOpen() : null
+
+  const overlayRef = useRef(null)
+  const sidebarRef = useRef(null)
+
   return open ? (
-    <div className={`${styles.sidebar} ${styles.sb}`}>
-      <button type='button' onClick={toggleOpen}>
-        Close
-      </button>
-      <ul>
-        <li>
-          <NavLink
-            activeClassName={styles.active}
-            exact
-            to='/'
-            onClick={toggleOpen}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            activeClassName={styles.active}
-            exact
-            to='/nemesis'
-            onClick={toggleOpen}
-          >
-            Nemesis
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            activeClassName={styles.active}
-            exact
-            to='/mages'
-            onClick={toggleOpen}
-          >
-            Mages
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            activeClassName={styles.active}
-            exact
-            to='/supply'
-            onClick={toggleOpen}
-          >
-            Supply
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            activeClassName={styles.active}
-            exact
-            to='/nemesis-cards'
-            onClick={toggleOpen}
-          >
-            Nemesis Cards
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            activeClassName={styles.active}
-            exact
-            to='/setup'
-            onClick={toggleOpen}
-          >
-            Setup
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            activeClassName={styles.active}
-            exact
-            to='/about'
-            onClick={toggleOpen}
-          >
-            About
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    <aside
+      className={styles.modal}
+      onClick={handleClickOutside}
+      ref={overlayRef}
+    >
+      <div className={`${styles.sidebar} ${styles.sb}`} ref={sidebarRef}>
+        <button type='button' onClick={toggleOpen}>
+          Close
+        </button>
+        <ul>
+          <li>
+            <NavLink
+              activeClassName={styles.active}
+              exact
+              to='/'
+              onClick={toggleOpen}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName={styles.active}
+              exact
+              to='/nemesis'
+              onClick={toggleOpen}
+            >
+              Nemesis
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName={styles.active}
+              exact
+              to='/mages'
+              onClick={toggleOpen}
+            >
+              Mages
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName={styles.active}
+              exact
+              to='/supply'
+              onClick={toggleOpen}
+            >
+              Supply
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName={styles.active}
+              exact
+              to='/nemesis-cards'
+              onClick={toggleOpen}
+            >
+              Nemesis Cards
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName={styles.active}
+              exact
+              to='/setup'
+              onClick={toggleOpen}
+            >
+              Setup
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName={styles.active}
+              exact
+              to='/about'
+              onClick={toggleOpen}
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </aside>
   ) : (
     <div className={`${styles.toggle} ${styles.sb}`}>
       <button className={styles.menuButton} type='button' onClick={toggleOpen}>
